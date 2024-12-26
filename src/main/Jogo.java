191cc8ba4d373474ae9f3a30b98772efd633f2c8
Lucas Duarte <lucasduartechaves@hotmail.com>
2024-12-25 21:22:08 -0300

    Cria função que calcula geração

--- src/main/Jogo.java
+++ src/main/Jogo.java
@@ -10,37 +10,82 @@ public class Jogo {
 	public Jogo() {
 		tabuleiro = new int[TAM_TABULEIRO][TAM_TABULEIRO];
 	}
-	
-	public void iniciar() {
-		gerarConfiguracaoInicial();
-		imprimirTabuleiroInicial();
-	}
 
-	private void gerarConfiguracaoInicial() {
+	public void iniciar() {
+		System.out.println();
+
 		for (int i = 0; i < TAM_TABULEIRO; i++) {
 			for (int j = 0; j < TAM_TABULEIRO; j++) {
 				tabuleiro[i][j] = obterNumeroAleatorio();
-			}
-		}
-	}
-	
-	private void imprimirTabuleiroInicial() {
-		System.out.println();
-		
-		for (int i = 0; i < TAM_TABULEIRO; i++) {
-			for (int j = 0; j < TAM_TABULEIRO; j++) {
 				System.out.print(tabuleiro[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
-	
+
 	private void gerarNovaConfiguracao() {
+
+		int[][] novoTabuleiro = new int[TAM_TABULEIRO][TAM_TABULEIRO];
+
 		for (int i = 0; i < TAM_TABULEIRO; i++) {
 			for (int j = 0; j < TAM_TABULEIRO; j++) {
-				//IMPLEMENTAR LÓGICA AQUI
+				
+				int qtdVizinhosVivos = calcularQuantidadeVizinhos(i,j);
+
+				// Qualquer célula com dois vizinhos vivos continua no mesmo estado para a
+				// próxima geração.
+				if (qtdVizinhosVivos == 2) {
+					novoTabuleiro[i][j] = tabuleiro[i][j];
+				} else {
+					if (tabuleiro[i][j] == 1) {
+
+						// Qualquer célula viva com menos de dois vizinhos vivos morre de solidão
+						if (qtdVizinhosVivos < 2) {
+							novoTabuleiro[i][j] = 0;
+						}
+
+						// Qualquer célula viva com mais de três vizinhos vivos morre de superpopulação.
+						if (qtdVizinhosVivos > 3) {
+							novoTabuleiro[i][j] = 0;
+						}
+
+					}
+
+					// Qualquer célula morta com exatamente três vizinhos vivos se torna uma célula
+					// viva.
+					if (tabuleiro[i][j] == 0 && qtdVizinhosVivos == 3) {
+						novoTabuleiro[i][j] = 1;
+					}
+
+				}
+
 			}
 		}
+
+		tabuleiro = novoTabuleiro;
+
+	}
+
+	private int calcularQuantidadeVizinhos(int i, int j) {
+
+		int qtdVizinhosVivos = 0;
+
+		int[] deslocamentoX = { -1, -1, -1, 0, 0, 1, 1, 1 };
+		int[] deslocamentoY = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+		for (int k = 0; k < 8; k++) {
+			int vizinhoCoordenadaX = i + deslocamentoX[k];
+			int vizinhoCoordenadaY = j + deslocamentoY[k];
+
+			if (vizinhoCoordenadaX >= 0 && vizinhoCoordenadaX < TAM_TABULEIRO && vizinhoCoordenadaY >= 0
+					&& vizinhoCoordenadaY < TAM_TABULEIRO) {
+
+				if (tabuleiro[vizinhoCoordenadaX][vizinhoCoordenadaY] == 1)
+					qtdVizinhosVivos++;
+			}
+		}
+
+		return qtdVizinhosVivos;
 	}
 
 	public void gerarRodada() {
@@ -53,7 +98,7 @@ public class Jogo {
 		}
 
 		gerarNovaConfiguracao();
-		
+
 		System.out.println();
 
 		for (int i = 0; i < TAM_TABULEIRO; i++) {
